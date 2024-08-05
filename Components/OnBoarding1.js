7965943a284302f8cde3df50b174d10feef23eb1
FatiXa00 <fatisahraoui01@gmail.com>
2024-08-05 15:21:17 +0100

    gehe

--- Components/OnBoarding1.js
+++ Components/OnBoarding1.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet, ImageBackground, Button, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 export default function Onboarding1() {
   const navigation = useNavigation();
 
   const handleButtonPress = () => {
-    // Navigate to the next screen, e.g., 'Login'
+    // Navigate to the next screen, e.g., 'OnBoarding2'
     navigation.navigate('OnBoarding2');
   };
 
@@ -17,12 +17,11 @@ export default function Onboarding1() {
     >
       <View style={styles.overlay}>
         <View style={styles.content}>
-          <Text style={styles.title}>Welcome to FitWave</Text>
           <Text style={styles.subtitle}>Start Your Fitness Adventure</Text>
         </View>
         <TouchableOpacity style={styles.button} onPress={handleButtonPress}>
-            <Text style={styles.buttonText}>Next</Text>
-          </TouchableOpacity>
+          <Text style={styles.buttonText}>Next</Text>
+        </TouchableOpacity>
       </View>
     </ImageBackground>
   );
@@ -37,37 +36,41 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Optional: overlay to darken the background
+    backgroundColor: 'rgba(0, 0, 0, 0.35)', // Optional: overlay to darken the background
   },
   content: {
+    marginTop:50,
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
+    padding: 50,
     backgroundColor: '#7E8385',
-    borderRadius: 10,
-    shadowColor: '#000',
+    shadowColor: '#7E8385',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.8,
     shadowRadius: 2,
     elevation: 5,
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    color:'red'
-  },
+ 
   subtitle: {
-    fontSize: 16,
-    marginBottom: 20,
+    fontSize: 18,
+    marginBottom: 18,
     textAlign: 'center',
-    color:'white'
+    color: 'white',
+    fontWeight: 'bold',
   },
   button: {
-    backgroundColor: '#007BFF',
+    marginTop: 30,
+    backgroundColor: 'rgba(255, 255, 255, 0.2)', // Transparent background
     paddingVertical: 12,
     paddingHorizontal: 25,
-    borderRadius: 5,
+    borderRadius: 100,
+    borderWidth: 1,
+    borderColor: 'rgba(255, 255, 255, 0.5)', // Semi-transparent border
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    elevation: 5, // Elevation for Android shadow
   },
   buttonText: {
     color: 'white',
