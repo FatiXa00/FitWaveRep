d7901159c3d440d4f6b2c1bfc314eb108e6b9140
Fatima Zahraa Sahraoui <110387237+FatiXa00@users.noreply.github.com>
2024-08-08 08:53:35 +0100

    Revert "testyy"

--- Components/Logging.js
+++ Components/Logging.js
@@ -1,6 +1,3 @@
-
-
-
 import React, {useState} from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image} from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
@@ -13,7 +10,6 @@ const Logging = ({ navigation }) => {
    const handleLogin = () => {
     navigation.navigate('Home'); // Replace 'Home' with the next screen after login
   };
-
 
   return (
     <View style={styles.container}>
